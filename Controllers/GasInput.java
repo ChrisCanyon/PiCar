c7f3447ea7db105f3fb34b2e7b8d7fc9d6fed8d8
chriscanyon <chrisnagy10@yahoo.com>
2017-09-26 13:46:03 -0400

    Better UI

--- Controllers/GasInput.java
+++ Controllers/GasInput.java
@@ -1,43 +1,26 @@
 package PiCar.Controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
-public class GasInput {
+public class GasInput{
 
     @FXML
-    private TextField actiontarget;
+    private Button submit;
 
+    @FXML
+    private TextField gasInput;
 
-    public void loadHandler(Stage stage, GridPane pane) {
-        actiontarget.setOnKeyPressed(event -> {
-            if(event.getCode().equals(KeyCode.ENTER)) {
-                System.out.println("doing stuff\n");
-                pushed(stage, actiontarget, pane);
-            }
-        });
+    @FXML
+    void submitData(ActionEvent event) {
+
     }
 
+    @FXML
+    void keyHandler(ActionEvent event) {
 
-    private void pushed(Stage stage, TextField userTextField, GridPane pane){
-        final Text text = new Text();
-        pane.add(text,2,1);
-        try {
-            Double x = Double.parseDouble(userTextField.getText());
-        } catch (NumberFormatException e) {
-            text.setFill(Color.RED);
-            text.setText("Error: Please enter a number");
-            userTextField.clear();
-            userTextField.requestFocus();
-            return;
-        }
-        text.setFill(Color.GREEN);
-        text.setText("Doing stuff!");
     }
 
 }
--- Controllers/HomeMenu.java
+++ Controllers/HomeMenu.java
@@ -1,6 +1,66 @@
 package PiCar.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
 
 public class HomeMenu {
 
+    @FXML
+    private Label mphDisplay;
+
+    @FXML
+    private MenuItem settings;
+
+    @FXML
+    private Label mpgDisplay;
+
+    @FXML
+    private Button tripReset;
+
+    @FXML
+    private Label tripDisplay;
+
+    @FXML
+    private ProgressBar fuelGuage;
+
+    @FXML
+    private LineChart<?, ?> mphChart;
+
+    @FXML
+    private MenuButton settingMenu;
+
+    @FXML
+    private MenuItem addGas;
+
+    @FXML
+    private AnchorPane inputPane;
+
+    @FXML
+    void keyHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resetTrip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadGasMenu(ActionEvent event) {
+
+    }
+
 }
+
